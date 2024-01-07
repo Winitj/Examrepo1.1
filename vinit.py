68fadcd8bd6cd63890340aa8927c138d1c5bf5d8
Winitj <vinitjain542003@gmail.com>
2024-01-08 00:26:18 +0530

    f

--- vinit.py
+++ vinit.py
@@ -28,16 +28,21 @@ base_mapping = {
     'B02836': 'Drinnen'
 }
 
-file_paths = [
-    "C://Users//jain vinit//Documents//FExam//uber-tlc-foil-response//uber-trip-data//uber-raw-data-aug14.csv", 
+file_paths = [  "C://Users//jain vinit//Documents//FExam//uber-tlc-foil-response//uber-trip-data//uber-raw-data-aug14.csv",
+           ]
+
+"""
+         
 
     "C://Users//jain vinit//Documents//FExam//uber-tlc-foil-response//uber-trip-data//uber-raw-data-jul14.csv", 
     "C://Users//jain vinit//Documents//FExam//uber-tlc-foil-response//uber-trip-data//uber-raw-data-jun14.csv", 
     "C://Users//jain vinit//Documents//FExam//uber-tlc-foil-response//uber-trip-data//uber-raw-data-may14.csv", 
     "C://Users//jain vinit//Documents//FExam//uber-tlc-foil-response//uber-trip-data//uber-raw-data-sep14.csv",
     "C://Users//jain vinit//Documents//FExam//uber-tlc-foil-response//uber-trip-data//uber-raw-data-apr14.csv", 
-    
-]
+"""
+
+#"C://Users//jain vinit//Documents//FExam//1.csv",
+ #            "C://Users//jain vinit//Documents//FExam//2.csv"
 
 
 ddf_list = [dd.read_csv(file) for file in file_paths]
@@ -53,7 +58,7 @@ cursor = connection.cursor()
 # Create the table if it doesn't exist
 create_table_query = """
 CREATE TABLE IF NOT EXISTS uber_data (
-    Date DATE,
+    Date DATETIME,
     Lat FLOAT,
     Lon FLOAT,
     Base VARCHAR(255)
@@ -68,7 +73,7 @@ connection.commit()
 engine = create_engine(f"mysql+mysqlconnector://{db_settings['user']}:{db_settings['password']}@{db_settings['host']}:{db_settings['port']}/{db_settings['database']}")
 
 
-df.compute().to_sql('uber_data', con=engine, if_exists='replace', index=False)
+df.compute().to_sql('uber_data3', con=engine, if_exists='replace', index=False)
 # Close the MySQL connection
 connection.close()
 cursor.close()
